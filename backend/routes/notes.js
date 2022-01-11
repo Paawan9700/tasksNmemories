59a145f7b1f297c6b3f3b1b1750c4705e6f315d0
Paawan Singal <paawansingal09@gmail.com>
2022-01-11 23:00:31 +0530

    end point for deleting a note

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const fetchuser = require('../middleware/fetchuser');
 const Note = require('../models/Note');
 const { body, validationResult } = require('express-validator');
+const { route } = require('./auth');
 
 //  ROUTE 1 : fetching all notes : {/api/notes/fetchnotes} -> ofc it require authetication
 router.get('/fetchnotes', fetchuser, async (req, res) => {
@@ -52,35 +53,70 @@ router.post('/addnote', fetchuser, [
 
 //  ROUTE 3 : update the required note : {/api/notes/updatenote} -> ofc it require authetication
 router.put('/updatenote/:id', fetchuser, async (req, res) => {
-    const { title, description, tag } = req.body;
 
-    // creating a newNote object
-    const newNote = {};
-    if (title) {
-        newNote.title = title
-    }
-    if (description) {
-        newNote.description = description;
-    }
-    if (tag) {
-        newNote.tag = tag;
-    }
+    try {
 
-    // what if, the note any person want to update does not exists
-    let note = await Note.findById(req.params.id);
-    if (!note) {
-        return res.status(404).send("not found");
-    }
+        const { title, description, tag } = req.body;
 
-    // now checking the user updating only his/her notes
-    if (req.user.id !== note.user.toString()) {
-        return res.status(401).send("not allowed");
-    }
+        // creating a newNote object
+        const newNote = {};
+        if (title) {
+            newNote.title = title
+        }
+        if (description) {
+            newNote.description = description;
+        }
+        if (tag) {
+            newNote.tag = tag;
+        }
 
-    // find the note to be updated and update it
-    const updatedNote = await Note.findByIdAndUpdate(req.params.id, { $set: newNote }, { new: true });
-    note = updatedNote;
-    res.json(note);
+        // what if, the note any person want to update does not exists
+        let note = await Note.findById(req.params.id);
+        if (!note) {
+            return res.status(404).send("not found");
+        }
+
+        // now checking the user updating only his/her notes
+        if (req.user.id !== note.user.toString()) {
+            return res.status(401).send("not allowed");
+        }
+
+        // find the note to be updated and update it
+        const updatedNote = await Note.findByIdAndUpdate(req.params.id, { $set: newNote }, { new: true });
+        note = updatedNote;
+        res.json(note);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Internal server error");
+    }
+});
+
+
+//  ROUTE 1 : deleting the  notes : {/api/notes/deletenote} -> ofc it require authetication
+router.delete('/deletenote/:id', fetchuser, async (req, res) => {
+
+    try {
+        // what if, the note any person want to delete does not exists
+        let note = await Note.findById(req.params.id);
+        if (!note) {
+            return res.status(404).send("not found");
+        }
+
+        // now checking the user deleting only his/her notes
+        if (req.user.id !== note.user.toString()) {
+            return res.status(401).send("not allowed");
+        }
+
+
+        // find the note to be deleted and delete it
+        const deleteNote = await Note.findByIdAndDelete(req.params.id);
+        note = deleteNote;
+        res.json({ "success": "note with the given id has been deleted", note: note });
+
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Internal server error");
+    }
 });
 
 module.exports = router;
